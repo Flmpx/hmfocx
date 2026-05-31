fa34ccdb1a297b26597093aad8ade82448f1e02b
Flmpx <flmpx18773906537@163.com>
2026-05-31 21:50:43 +0800

    feat: add base structure to this libiray

--- base.h
+++ base.h
@@ -2,13 +2,22 @@
 #define BASE_H
 
 
-typedef struct hm_data {
-    void* data;
-    void* content;
-    bool isOwner;
-    bool isEmpty;
-} hm_data;
+/// @brief result of compare
+typedef enum hm_cmp_res {
+    less = -1,
+    same,
+    more
+} hm_cmp_res;
 
+
+
+/// @brief return information of functions
+typedef enum hm_return_info {
+    hm_error = 2,
+    hm_warning,
+    hm_none,
+    hm_success
+} hm_return_info;
 
 
 
