86ce2ab6149963a4aba34564cc67639b1146e28f
Flmpx <flmpx18773906537@163.com>
2026-06-01 22:58:13 +0800

    feat: add new basic enum and some basic functions to this library

--- base.h
+++ base.h
@@ -4,21 +4,23 @@
 
 /// @brief result of compare
 typedef enum hm_cmp_res {
-    less = -1,
-    same,
-    more
+    hm_less = -1,
+    hm_same,
+    hm_more
 } hm_cmp_res;
 
 
 
 /// @brief return information of functions
-typedef enum hm_return_info {
+typedef enum hm_info {
     hm_error = 2,
     hm_warning,
     hm_none,
     hm_success
-} hm_return_info;
+} hm_info;
 
 
+
+typedef void (*hm_free)(void* ptr);
 
 #endif
