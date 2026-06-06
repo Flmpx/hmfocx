7cb95af961aa32b1d852f2fd228a965d03e92660
Flmpx <flmpx18773906537@163.com>
2026-06-06 21:06:59 +0800

    feat: Add some function pointer typedef for some function
    1. hm_cmp_res: the result of comparing datas of ptr1 and ptr2
    2. hm_hash: the hash result of the data of ptr

--- base.h
+++ base.h
@@ -1,6 +1,6 @@
 #ifndef BASE_H
 #define BASE_H
-
+#include <stdio.h>
 
 /// @brief result of compare
 typedef enum hm_cmp_res {
@@ -22,5 +22,11 @@ typedef enum hm_info {
 
 
 typedef void (*hm_free)(void* ptr);
+
+typedef hm_cmp_res (*hm_cmp)(void* ptr1, void* ptr2);
+
+typedef size_t (*hm_hash)(void* ptr);
+
+
 
 #endif
