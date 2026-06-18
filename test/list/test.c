6a0f1c9234c9fcda9daf34b2fd8acf39a8fd756b
Flmpx <flmpx18773906537@163.com>
2026-06-18 18:33:59 +0800

    test(list): add stressful del tail test in list
    - the type of value is `int`
    - the value is located in stack memory of system

--- test/list/test.c
+++ test/list/test.c
@@ -879,6 +879,40 @@ void test_list_del_head_stress() {
     print_end("DEL HEAD STRESS TEST | TYPE: [INT]", fail_cnt);
 }
 
+void test_list_del_tail_stress() {
+    int fail_cnt = 0;
+    int v = 666666;
+    hm_list list;
+    hm_list_init(&list, NULL);
+    size_t nums[] = {500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000};
+    int cnt = sizeof(nums) / sizeof(size_t);
+
+    print_run("DEL TAIL STRESS TEST | TYPE: [INT]");
+    for (int i = 0; i < cnt; i++) {
+        // insert
+        for (size_t j = 0; j < nums[i]; j++) {
+            hm_list_insert_tail(&list, &v);
+        }
+
+        // del
+
+        int fail_del = 0;
+        
+        clock_t start = clock();
+        for (size_t j = 0; j < nums[i]; j++) {
+            if (hm_list_del_tail(&list) != hm_list_ret_suc) {
+                fail_del++;
+            }
+        }
+        clock_t end = clock();
+        check_res(fail_del == 0, "the tag of return isn't suc when run stressful del tail test", &fail_cnt);
+        print_run_time("DEL", start, end, nums[i], nums[i]);
+        hm_list_free(&list);
+
+    }
+    print_end("DEL TAIL STRESS TEST | TYPE: [INT]", fail_cnt);
+}
+
 
 int main()
 {
@@ -910,6 +944,8 @@ int main()
     test_list_get_stress();                         printf("\n");
 
     test_list_del_head_stress();                    printf("\n");
+
+    test_list_del_tail_stress();                    printf("\n");
     
     srand(time(NULL));
     return 0;
