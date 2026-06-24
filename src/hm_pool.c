a819422fbf9973ae1c7ca6e77f3bbdf62794d7b3
Flmpx <flmpx18773906537@163.com>
2026-06-24 12:14:41 +0800

    feat(pool): Add new container for this library -- `hm_pool`
    - This container is a memory pool with fixed-size block.
    - It include some basic function, such as `allocate` `free` .
    - Warning: this container hasn't been tested at all.

---
+++ src/hm_pool.c
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2026 Flmpx
+ * Licensed under MIT (see LICENSE).
+ */
+
+#include "../include/hm_pool.h"
+#include <stdlib.h>
+
+/**
+ * Align the memory to the variable `align`
+ */
+static size_t align_up(size_t n, size_t align) {
+    return (n + (align - 1) & ~(align - 1));
+}
+
+
+
+/**
+ * Initialize the memory pool 
+ * @note - `hm_pool_block_allocate` will reurn a valid pointer when the `block_size` is `zero` because memory alignment
+ * @note - `hm_pool_block_allocate` will return `NULL` if `blocks_per_page` is `zero` 
+ */
+void hm_pool_init(hm_pool* pool, size_t block_size, size_t blocks_per_page) {
+    pool->head_page = pool->head_block = NULL;
+
+    pool->blocks_per_page = blocks_per_page;
+    
+    block_size = block_size > sizeof(hm_pool_block_node) ? block_size : sizeof(hm_pool_block_node);
+    pool->block_size = align_up(block_size, sizeof(void*));
+
+}
+
+
+/**
+ * Get pointer of a block
+ * @note - It will return `NULL` when allocate failed
+ */
+void* hm_pool_block_allocate(hm_pool* pool) {
+
+    if (pool->head_block) {
+        hm_pool_block_node* node = pool->head_block;
+        pool->head_block = pool->head_block->next;
+        return node;
+    }
+
+
+    hm_pool_page_node* new_page = (hm_pool_page_node*)malloc(sizeof(hm_pool_page_node) + pool->block_size * pool->blocks_per_page);
+
+    if (new_page == NULL) {
+        return NULL;
+    }
+
+    new_page->next = pool->head_page;
+    pool->head_page = new_page;
+
+    for (size_t i = 0; i < pool->blocks_per_page * pool->block_size; i += pool->block_size) {
+        hm_pool_block_node* node = (char*)(new_page + 1) + i;
+        node->next = pool->head_block;
+        pool->head_block = node;
+    }
+
+    hm_pool_block_node* node = pool->head_block;
+
+    /**
+     * the next action will generate error if the blocks_per_page is zero
+     * So, let it return NULL for user
+     */
+    if (pool->head_block) {
+        pool->head_block = pool->head_block->next;
+    }
+
+    return node;
+}
+
+
+/**
+ * Free a block
+ * @note - The pointer of block must match to the memory pool
+ */
+void hm_pool_block_free(hm_pool* pool, void* block) {
+    hm_pool_block_node* node = (hm_pool_block_node*)block;
+    node->next = pool->head_block;
+    pool->head_block = node;
+}
+
+
+/**
+ * Free the all content of memory pool
+ * @note You don't use the block from a freed memory pool
+ */
+void hm_pool_free(hm_pool* pool) {
+    hm_pool_page_node* node = pool->head_page;
+    while (node) {
+        free(node);
+        node = node->next;
+    }
+    hm_pool_init(pool, pool->block_size, pool->blocks_per_page);
+}
+
+
+/**
+ * Get the number of pages in the memory pool that passed in
+ * 
+ */
+size_t hm_pool_get_pages(hm_pool* pool) {
+    size_t cnt = 0;
+    hm_pool_page_node* cur;
+    while (cur) {
+        cur = cur->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+
+/**
+ * Get the theoretical number of bytes of every page in the memory pool
+ * 
+ */
+size_t hm_pool_get_bytes_per_page(hm_pool* pool) {
+    return sizeof(hm_pool_page_node) + pool->block_size * pool->blocks_per_page;
+}
