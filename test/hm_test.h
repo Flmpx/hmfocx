2eead24cea34e186fdaab857659f13baf77e98de
Flmpx <flmpx18773906537@163.com>
2026-06-27 19:32:57 +0800

    test: Add print function of `print_speed_vs`
    - This can compare the speed of operation

--- test/hm_test.h
+++ test/hm_test.h
@@ -11,4 +11,7 @@ extern void check_res(bool res, const char* info, int* fail_cnt);
 extern void print_run(const char* info);
 extern void print_end(const char* info, int fail_cnt);
 extern void print_run_time(const char* info, clock_t start, clock_t end, size_t scale, size_t oper_cnt);
+extern void print_speed_vs(const char* info_a, clock_t start_a, clock_t end_a,
+                           const char* info_b, clock_t start_b, clock_t end_b,
+                           size_t scale, size_t oper_cnt);
 #endif
