8860a7e426c300e9c174dd54c2e49395359e250e
Flmpx <flmpx18773906537@163.com>
2026-07-07 10:51:31 +0800

    feat(queue): Init the all file linked to queue

---
+++ test/queue/test.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2026 Flmpx
+ * Licensed under MIT (see LICENSE).
+ */
+
+#include "../../include/hm_list.h"
+#include "../../function/cmp/hm_cmp.h"
+#include "../hm_test.h"
+
+// This variable can record the total number of failures and it can be used as a return value to check whether the test passed
+int all_failure_num = 0;
+
+// use a macro to replace the repetitive code 
+#define HM_TEST_COUNTER \
+    all_failure_num += fail_cnt;
+
+
+// every test function ...
+
+void function_test() {
+    
+}
+
+void boundary_test() {
+    
+}
+
+
+int main()
+{
+    // Group the test roughly
+    function_test();
+    boundary_test();
+    return all_failure_num;
+}
+
